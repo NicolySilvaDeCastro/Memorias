98d8978e89b6272846afc3b0569fd31fecd1a2b7
Nicoly Silva de Castro <nicolyscastro@gmail.com>
2024-10-15 23:13:07 -0300

    atualização

--- app/adicionar/index.jsx
+++ app/adicionar/index.jsx
@@ -1,20 +1,118 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
+import React, { useState } from 'react';
+import { Link } from 'expo-router';
+import { View, Text, StyleSheet, TextInput, Pressable, Image } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function Add() {
+  // isso define os estados das variáveis
+  const [titulo, setTitulo] = useState('');
+  const [data, setData] = useState('');
+  const [local, setLocal] = useState('');
+  const [desc, setDesc] = useState('');
+  const [imageUri, setImageUri] = useState('');
+
+  //essa parte irá selecionar a imagem utilizando o image picker
+  const selecionarImagem = async () => {
+  //permissão para acessar a galeria
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.granted === false) {
+      alert('É necessário permitir o acesso à galeria!');
+      return;
+    }
+
+    // Abrir a galeria
+    const pickerResult = await ImagePicker.launchImageLibraryAsync();
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setImageUri(pickerResult.uri);
+  };
+
+  //"basicamente" salva as memórias
+  const salvarMemoria = async () => {
+    const novaMemoria = { titulo, data, local, desc, imageUri };
+    try {
+      const listaMemorias = await AsyncStorage.getItem('lista_memorias'); //recupera memórias na asyncStorage
+      const memoriaAtual = listaMemorias ? JSON.parse(listaMemorias) : []; // Verifica se listaMemorias contém dados, se houver dados transforma o json em string para que seja lido
+      memoriaAtual.push(novaMemoria);  //Adiciona o objeto novaMemoria ao array memoriaAtual
+      await AsyncStorage.setItem('lista_memorias', JSON.stringify(memoriaAtual));  //Utiliza AsyncStorage.setItem para armazenar a lista
+      alert('Memória adicionada com sucesso!');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Adicionar Memória</Text>
+      <TextInput placeholder='Título' onChangeText={setTitulo} style={styles.input} />
+      <TextInput placeholder='Data' onChangeText={setData} style={styles.input} />
+      <TextInput placeholder='Local' onChangeText={setLocal} style={styles.input} />
+      <TextInput placeholder='Descrição' onChangeText={setDesc} style={styles.input} />
+
+      <Pressable onPress={selecionarImagem} style={styles.button}>
+        <Text style={styles.buttonText}>Selecionar Imagem</Text>
+      </Pressable>
+
+      {imageUri ? <Image source={{ uri: imageUri }} style={styles.imagePreview} /> : null}
+
+      <Pressable onPress={salvarMemoria} style={styles.button}>
+        <Text style={styles.buttonText}>Salvar Memória</Text>
+      </Pressable>
+
+      <Link href="/memorias">
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Voltar</Text>
+        </Pressable>
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    backgroundColor: '#c99066',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    backgroundColor: '#eacabe',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: 'black',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: 'white',
+  },
+  titulo: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  imagePreview: {
+    width: 100,
+    height: 100,
+    marginVertical: 10,
+    borderRadius: 10,
+  },
+});
 
 
 
-
-export default function Home() {
-    const [value, setValue] = React.useState('');
- 
-    const onChange = event => {
-      localStorage.setItem('myValueInLocalStorage', event.target.value);
-   
-      setValue(event.target.value);
-    };
-
-    // const [gravei, setGravei] = useState('')
+//RASCUNHOS:
+ // const [gravei, setGravei] = useState('')
     // async function setData(){
     //     await AsyncStorage.setItem("Var Gravada", gravei)
     // }
@@ -36,45 +134,59 @@ export default function Home() {
 //     }
 //     lista_memorias = asyncStorage.getItem('lista_memorias')
 //  }
-  return (
-    <View style={styles.container}>
-        <View>
-        <Text>Memórias</Text> 
-        <TextInput value={value} type="text" onChange={onChange} />
- 
-        <Text>{value}</Text>
-        {/* <TextInput 
-        value={gravei}
-        placeholder='Digita ae'
-        onChangeText={newGravei => setGravei(newGravei)} defaultValue={gravei}/> */}
-        {/* <TextInput placeholder='Quando aconteceu?'></TextInput>
-        <TextInput placeholder='O que aconteceu?'></TextInput>
-        <TextInput placeholder='Descrição'></TextInput> */}
-        {/* <Button style={styles.botao} title="Adicionar Memória" /> */}
-    </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%'
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: 'white'
-  },
-  botao: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black',
-    color: 'white'
-  },
-});
 
 //https://www.linkedin.com/pulse/local-storage-react-native-f%C3%A1cil-e-simples-daniel-rodrigues/
 //https://josiaspereira.com.br/como-usar-localstorage-no-reactjs/
+
+// import React, { useState } from 'react';
+// import { View, Text, Button, StyleSheet, TextInput } from 'react-native';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+
+
+// export default function Add() {
+//     const [value, setValue] = React.useState('');
+ 
+//     const onChange = event => {
+//       localStorage.setItem('myValueInLocalStorage', event.target.value);
+   
+//       setValue(event.target.value);
+//     };
+
+
+//   return (
+//     <View style={styles.container}>
+//         <View>
+//         <Text>Memórias</Text> 
+//         <View>
+//         <TextInput placeholder='Título' type="text" onChange={onChange} />
+//         <TextInput placeholder='Data' type="text" onChange={onChange} />
+//         <TextInput placeholder='Local' type="text" onChange={onChange} />
+//         <TextInput placeholder='Descrição' type="text" onChange={onChange} />
+//         </View>
+
+//     </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     width: '100%'
+//   },
+//   title: {
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//     color: 'white'
+//   },
+//   botao: {
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: 'black',
+//     color: 'white'
+//   },
+// });
--- app/memorias/index.jsx
+++ app/memorias/index.jsx
@@ -1,35 +1,202 @@
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Pressable, ImageBackground } from 'react-native';  
 import { Link } from 'expo-router';
+import { useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+//Essa é uma função assíncrona que tenta obter uma lista de memórias armazenadas no AsyncStorage
+const getStoredMemories = async () => {
+  try {
+    const value = await AsyncStorage.getItem('lista_memorias');
+    return value ? JSON.parse(value) : []; // o parse tranformará um arquivo json em string para que possa ser llido
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+};
 
 export default function Home() {
+  const [memorias, setMemorias] = useState([]);
+
+  useEffect(() => {
+    const fetchMemories = async () => { //a função fetchMemories pega as memórias e as exibe de forma assincrona
+      const memories = await getStoredMemories();
+      setMemorias(memories);
+    };
+    fetchMemories();
+  }, []);
+
   return (
+    <ImageBackground
+    source={{ uri: 'https://i.pinimg.com/originals/cc/f3/40/ccf340c6ec1740f8f0ac4332843ca348.jpg' }} 
+    style={styles.background}
+    resizeMode="cover"
+  >
     <View style={styles.container}>
-    <View>
-        <Text>Memórias</Text> 
-        <Link href="/adicionar">
-        <Button style={styles.botao} title="Adicionar Memória" />
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Minhas Memórias</Text>
+      </View>
+      <FlatList
+        data={memorias}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.memoryItem}>
+            <Text style={styles.title}>{item.titulo}</Text>
+            <Text>{item.data}</Text>
+            <Text>{item.local}</Text>
+            <Text>{item.desc}</Text>
+            {item.imageUri ? <Image source={{ uri: item.imageUri }} style={styles.imagePreview} /> : null}
+          </View>
+        )}
+      />
+      <Link href="/adicionar">
+        <Pressable style={styles.butao}>
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </Pressable>
       </Link>
     </View>
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
     justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
     alignItems: 'center',
-    width: '100%'
+    width: '100%',
+  },
+  header: {
+    width: '100%',
+    padding: 20,
+    gap: 20,
+    backgroundColor: 'black', 
+    alignItems: 'center',
+  },
+  headerTitle: {
+    fontSize: 30,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  memoryItem: {
+    padding: 10,
+    backgroundColor: '#f9f9f9',
+    borderRadius: 5,
+    width: '100%', 
+    marginBottom: 15,
   },
   title: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: 'bold',
-    color: 'white'
   },
-  botao: {
+  butao: {
+    backgroundColor: 'brown',
     justifyContent: 'center',
+    width: '110%',
     alignItems: 'center',
-    backgroundColor: 'black',
-    color: 'white'
+    borderRadius: 10
   },
+  buttonText: {
+    color: 'white',
+    fontSize: 25,
+    fontWeight: 'bold'
+  },
+  imagePreview: {
+    width: '100%',
+    height: 200,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  
 });
+
+
+
+
+//RASCUNHOS:
+
+// const tela = (() =>{
+//   const [memorias, setMemorias] = useState([])
+  
+//   let result = getStoredMemories()
+//   if (result){
+//     setMemorias(JSON.parse(result)) //stringify transforma o json em string, parse
+//   }
+// },[])
+
+
+
+
+// import { View, Text, Button, StyleSheet, FlatList, Image } from 'react-native';
+// import { Link } from 'expo-router';
+// import { useState, useEffect } from 'react';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// const getStoredMemories = async () => {
+//   try {
+//     const value = await AsyncStorage.getItem('lista_memorias');
+//     return value ? JSON.parse(value) : []; // Retorna um array vazio se não houver memórias
+//   } catch (error) {
+//     console.log(error);
+//     return [];
+//   }
+// };
+
+
+// const tela = (() =>{
+//   const [memorias, setMemorias] = useState([])
+  
+//   let result = getStoredMemories()
+//   if (result){
+//     setMemorias(JSON.parse(result)) //stringify transforma o json em string, parse
+//   }
+// },[])
+
+// export default function Home() {
+//   return (
+//     <View style={styles.container}>
+//     <View>
+//         <Text>Memórias</Text> 
+//         <Link href="/adicionar">
+//         <Button title="Adicionar Memória" />
+//       </Link>
+//       <FlatList
+//       data= {memorias}
+//       renderItem={({ item }) => (
+//         <View>
+//         <Image source ={{ uri: item.image }}/>
+//         <Text>{item.titulo}</Text>
+//         <Text>{item.data}</Text>
+//         <Text>{item.local}</Text>
+//         <Text>{item.desc}</Text>
+//         </View>
+//         )} 
+//       />
+//     </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     width: '100%'
+//   },
+//   title: {
+//     fontSize: 24,
+//     fontWeight: 'bold',
+//     color: 'white'
+//   },
+//   botao: {
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: 'black',
+//     color: 'white'
+//   },
+// });
+
